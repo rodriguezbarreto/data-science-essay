1f643b3b308ef28312538ebc790a8a76441eaebd
Daniel Barreto <munizbarreto5@gmail.com>
2021-12-25 17:13:53 -0300

    feat: implement sanitizator

--- src/app.py
+++ src/app.py
@@ -1,1 +1,8 @@
-print('olá mundo')
+from sanitizator.sanitizator import sanitizator
+
+origin = 'database/origin'
+destiny = 'database/new'
+
+
+# Receives origin folder and destination folder path.And sanitizes tables.
+sanitizator(origin, destiny)
--- src/sanitizator/sanitizator.py
+++ src/sanitizator/sanitizator.py
@@ -1,2 +1,11 @@
-def sanitizator():
-    return 'olá mundo'
+import pandas as pd
+import os
+
+
+def sanitizator(origin, destiny):
+    print(origin)
+    tables = os.listdir(origin)
+    for table in tables:
+        new_table = pd.read_csv(f'{origin}/{table}', on_bad_lines='skip')
+        df = pd.DataFrame(new_table)
+        df.to_csv(f'{destiny}/new_{os.path.basename(table)}')
